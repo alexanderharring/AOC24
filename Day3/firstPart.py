f41daf302ba848d9bd210cf1981d1614b1fb01ae
alexa <alexa@LAPTOP-SMKPBBBG>
2024-12-03 12:07:50 +0000

    oops

--- Day3/firstPart.py
+++ Day3/firstPart.py
@@ -7,7 +7,7 @@ with open("input.txt") as files:
     x = re.findall("mul\(-?\d+,\-?\d+\)", data)
     
     bigsum = 0
-    
+     
     for z in x:
         
         nms = [int(p) for p in z[4:-1].split(",")]
